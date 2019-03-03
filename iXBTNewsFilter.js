b072477bacdc96ca573c1d379b4f41b167f16161
AlexRabinovitch <36998067+AlexRabinovitch@users.noreply.github.com>
2019-03-03 09:33:43 +0200

    Update iXBTNewsFilter.js

--- iXBTNewsFilter.js
+++ iXBTNewsFilter.js
@@ -1,1 +1,64 @@
+// ==UserScript==
+// @name         iXBT news filter
+// @namespace    https://tampermonkey.net/
+// @version      0.1
+// @description  Removes annoying news and comments
+// @author       Alex Rabinovitch
+// @match        https://www.ixbt.com/news*
+// @require      http://code.jquery.com/jquery-latest.js
+// @require      https://gist.github.com/raw/2625891/waitForKeyElements.js
+// @grant        none
+// ==/UserScript==
 
+(function() {
+    'use strict';
+
+    var bannedItems = ["Xiaomi", "Oukitel", "Leagoo"];
+
+    try
+    {
+        var articles = $('div.item.no-padding');
+
+        articles.each(function(i, article) {
+            var headers = $(article).find('h2').clone();
+
+            headers.each(function(j, header) {
+                var headerText = header.innerText;
+                if (new RegExp(bannedItems.join("|")).test(headerText))
+                {
+                    $(article).remove();
+                }
+            });
+        });
+    }
+    catch(e)
+    {
+        console.log('Error: ' + e.message);
+    }
+})();
+
+waitForKeyElements ("section.comment", filterComments);
+
+function filterComments(jnode)
+{
+    console.log('filterComments');
+
+    var bannedItems = ["Вызов девушек", "подтянуть живот", "Кому нужны проститутки"];
+
+    try
+    {
+        var comments = jnode.find('div.comment-content.text');
+        comments.each(function(i, comment)
+        {
+            var commentText = comment.innerText;
+            if (new RegExp(bannedItems.join("|")).test(commentText))
+            {
+                jnode.remove();
+            }
+        });
+    }
+    catch(e)
+    {
+        console.log('Error: ' + e.message);
+    }
+}
