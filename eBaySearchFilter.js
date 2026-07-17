4e89db70d6f2f9f6ef831348770bbceb2fc9f7a4
AlexRabinovitch <36998067+AlexRabinovitch@users.noreply.github.com>
2026-07-17 10:35:42 +0300

    Add Dulcop knights filter to eBaySearchFilter

--- eBaySearchFilter.js
+++ eBaySearchFilter.js
@@ -486,6 +486,7 @@ function fillFilters()
     // DZI knights // worldwide // (soviet,ussr,dzi,dfi,donetsk) (knight,knights) -(chess,metal,tin,su,mig,1/72)
     // Elastolin stagecoach // worldwide // elastolin (postkutsche,kutsche,stage,stagecoach) -(lineol,masse,kübel,flachwagen,personenwagen,jeep,tipp,krupp,wk,1935,1938,4cm)
     // Rusty Kern books based on Playset Magazine // worldwide // (marx) (kern) -(karl,bruno) // ihaveitusa,ihaveit_music,ihaveitusa,ihaveit_music,rarewaves-usa,ihaveitusa,ihaveit_music,ihaveitusa,ihaveit_music,rarewaves-usa,rarewaves-europe,ihaveitusa,ihaveit_music,ihaveitusa,ihaveit_music,rarewaves-usa,ihaveitusa,ihaveit_music,ihaveitusa,ihaveit_music,rarewaves-usa,rarewaves-europe,rarewaves,ihaveitusa,ihaveit_music,ihaveitusa,ihaveit_music,rarewaves-usa,ihaveitusa,ihaveit_music,ihaveitusa,ihaveit_music,rarewaves-usa,rarewaves-europe,ihaveitusa,ihaveit_music,ihaveitusa,ihaveit_music,rarewaves-usa,ihaveitusa,ihaveit_music,ihaveitusa,ihaveit_music,rarewaves-usa,rarewaves-europe,rarewaves,rarewaves-ca,newlyolder
+    // Dulcop knights // worldwide // dulcop (knight,knights,cavaliere,cavalieri,crusade,crusades,crociate,medieval,medievali) -(supercharly) // maryjack2012,franinca1
 
     console.log('Done filling filters.');
 }
